99e81183dfc0948094a9fa84d22d1928d15944ae
Ercan Karahan <ercankarahan@hotmail.de>
2020-10-19 21:47:41 +0300

    Add project files.

---
+++ WFA_EntityFrameWork_Odev/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WFA_EntityFrameWork_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblIsim = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(35, 150);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(35, 266);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.ItemHeight = 16;
+            this.lstProducts.Location = new System.Drawing.Point(156, 34);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(223, 372);
+            this.lstProducts.TabIndex = 2;
+            this.lstProducts.Click += new System.EventHandler(this.lstProducts_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(35, 382);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(424, 82);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategory.TabIndex = 5;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(422, 39);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(73, 17);
+            this.lblCategory.TabIndex = 6;
+            this.lblCategory.Text = "Category :";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(35, 34);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(424, 175);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(425, 266);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 22);
+            this.txtPrice.TabIndex = 8;
+            // 
+            // lblIsim
+            // 
+            this.lblIsim.AutoSize = true;
+            this.lblIsim.Location = new System.Drawing.Point(421, 132);
+            this.lblIsim.Name = "lblIsim";
+            this.lblIsim.Size = new System.Drawing.Size(40, 17);
+            this.lblIsim.TabIndex = 9;
+            this.lblIsim.Text = "Isim :";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(422, 223);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(48, 17);
+            this.lblPrice.TabIndex = 10;
+            this.lblPrice.Text = "Price :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblIsim);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lstProducts);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblIsim;
+        private System.Windows.Forms.Label lblPrice;
+    }
+}
+
---
+++ WFA_EntityFrameWork_Odev/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_EntityFrameWork_Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities _db = new NorthwindEntities();
+        Product modimodifiedProduct;
+        void ProductList()
+        {
+            lstProducts.DataSource = _db.Products.ToList();
+            lstProducts.DisplayMember = "ProductName";
+            lstProducts.SelectedIndex = -1;
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            ProductList();
+        }
+
+        void Clear()
+        {
+            txtPrice.Text = null;
+            txtName.Text = null;
+            cmbCategory.SelectedIndex = -1;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product p=new Product();
+            p.Category = cmbCategory.SelectedItem as Category;
+            p.ProductName = txtName.Text;
+            p.UnitPrice = Convert.ToDecimal(txtPrice.Text);
+            _db.Products.Add(p);
+            _db.SaveChanges();
+            Clear();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCategory.DataSource = _db.Categories.ToList();
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.SelectedIndex = -1;
+        }
+
+        private void lstProducts_Click(object sender, EventArgs e)
+        {
+            if (lstProducts.SelectedIndex > -1) 
+            {
+                modimodifiedProduct=lstProducts.SelectedItem as Product;
+                txtName.Text = (lstProducts.SelectedItem as Product).ProductName;
+                txtPrice.Text = (lstProducts.SelectedItem as Product).UnitPrice.ToString();
+                cmbCategory.SelectedItem = (lstProducts.SelectedItem as Product).Category;
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (modimodifiedProduct is null)
+            {
+                MessageBox.Show("Plaese select a Item to Update");
+                return;
+            }
+
+            modimodifiedProduct.ProductName = txtName.Text;
+            modimodifiedProduct.UnitPrice = Convert.ToDecimal(txtPrice.Text);
+            modimodifiedProduct.Category = cmbCategory.SelectedItem as Category;
+            modimodifiedProduct = null;
+            _db.SaveChanges();
+            ProductList();
+            Clear();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (modimodifiedProduct is null)
+            {
+                MessageBox.Show("Please select Deleted Itenm");    
+                return;
+            }
+            _db.Products.Remove(modimodifiedProduct);
+            modimodifiedProduct = null;
+            _db.SaveChanges();
+            ProductList();
+            Clear();
+        }
+    }
+
+    
+}
